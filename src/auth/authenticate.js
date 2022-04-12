0148a13b08f795fb105be231c1b7deee340c839c
MaximilianoSpaliansky <usr2254@salesianos-lacuesta.net>
2022-04-12 12:18:36 +0100

    Feature: Authenticate By Token

---
+++ src/auth/authenticate.js
@@ -0,0 +1,38 @@
+require("dotenv").config();
+
+const moment = require("moment");
+const jwt = require("jwt-simple");
+
+const buildToken = (user) => {
+  const payload = {
+    userId: user.id,
+    createdAt: moment().unix(),
+    expiredAt: moment().add(20, "minutes").unix(),
+  };
+  return jwt.encode(payload, process.env.SECRET_WORD);
+};
+
+const authByToken = (req, res, next) => {
+  if (!req.headers["Authenticate"]) {
+    return res.json({ error: "Missing Token" });
+  }
+
+  const userToken = req.headers["Authenticate"];
+  let payload = {};
+
+  try {
+    payload = jwt.decode(userToken, process.env.SECRET_WORD);
+  } catch (error) {
+    return res.json({ error: "Invalid Token" });
+  }
+
+  if (payload.expiredAt < moment().unix()) {
+    return res.json({ error: "Expired Token" });
+  }
+
+  req.userId = payload.userId;
+
+  next();
+};
+
+module.exports = { buildToken, authByToken };
